3c94beb2b1ce8620f1b49bc6dc967f24dd61398b
Samy THELLIER <st@samys-macbook-air.local>
2024-11-20 13:45:37 -0500

    Fix port configuration and streamline app deployment

--- app.py
+++ app.py
@@ -7,10 +7,3 @@ load_dotenv()
 from legatera import create_app
 
 app = create_app()
-
-if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        port=int(os.getenv('PORT', 8080)),
-        debug=os.getenv('FLASK_ENV') == 'development'
-    )
